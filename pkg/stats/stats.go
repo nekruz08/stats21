03315f6c266e9034e0d71ba2763b7cd4b0fdc67f
Nekruz <nekruzihikmatullo@gmail.com>
2021-12-25 11:47:39 +0300

    добавлена функция FilterByCategory и тесты к нему

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -29,3 +29,15 @@ func TotalInCategory(payments []types.Payment, category types.Category) types.Mo
 	}
 	return sum
 }
+
+// FilterByCategory возвращает платежи в указанной категории.
+func FilterByCategory(payments []types.Payment, category types.Category) []types.Payment {
+	var filtered []types.Payment
+	for _, payment := range payments {
+		if payment.Category==category{
+			filtered = append(filtered, payment)
+		}
+	}
+	
+	return filtered
+}
